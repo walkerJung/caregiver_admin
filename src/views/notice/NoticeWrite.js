4f9f86283c76eb298b4e7661c85e91ab5a434fe9
wise3007 <50983929+wise3007@users.noreply.github.com>
2021-11-19 15:52:16 +0900

    21.11.19 (15:52)

---
+++ src/views/notice/NoticeWrite.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  Input,
+  ButtonGroup,
+  Button,
+} from "reactstrap";
+
+function NoticeWrite() {
+  return (
+    <>
+      <div className="content">asdasd</div>
+    </>
+  );
+}
+
+export default NoticeWrite;
