78c38c3d7f355f4a414c48c628e0b794b741a96c
걷는사람 정정훈 <92363576+walkerJung@users.noreply.github.com>
2021-12-27 12:20:08 +0900

    2021-12:27 12:20

--- src/config/Env.js
+++ src/config/Env.js
@@ -1,1 +1,2 @@
-export const graphqlUrl = "http://3.36.22.165:4000/graphql";
+// export const graphqlUrl = "http://3.36.22.165:4000/graphql";
+export const graphqlUrl = "http://api.care-korea.kr/graphql";
